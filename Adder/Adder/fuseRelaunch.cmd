dd98e5440e972e37cf45517028438d446fa8a10e
Siva-Datta <51356568+Siva-Datta@users.noreply.github.com>
2020-03-14 16:48:31 +0530

    Adder

---
+++ Adder/Adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "G:/Xilinx projects/Adder/adder_test_isim_beh.exe" -prj "G:/Xilinx projects/Adder/adder_test_beh.prj" "work.adder_test" "work.glbl" 
